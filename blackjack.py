ed659d123d91ae07b1efbe093acec16612182af0
Ghouljokes <gastreich.j@gmail.com>
2021-11-10 13:25:08 -0800

    move deck to its own file

--- blackjack.py
+++ blackjack.py
@@ -2,31 +2,7 @@ import random
 import copy
 import time
 from dudesrock import Dude
-
-"""Stuff I wanna add:
-    option for npc player(s)
-    split
-Stuff I need to do:
-    organize the code relating to playing the game
-"""
-deck = []
-class Card:
-    def __init__(self, value: int, name: str, suit: str) -> None:
-        self.value = value
-        self.name = name
-        self.suit = suit
-        self.in_deck = True
-    
-    def get_full_name(self) -> str:
-        return f"{self.name} of {self.suit}"
-
-
-for suit in ["hearts", "diamonds", "spades", "clubs"]:
-    deck.append(Card(11, "Ace", suit))
-    for i in range(2, 11):
-        deck.append(Card(i, str(i), suit))
-    for face in ["Jack", "Queen", "King"]:
-        deck.append(Card(10, face, suit))
+from deck import deck as deck
 
 
 class User(Dude):
@@ -48,11 +24,6 @@ class User(Dude):
 
 dealer = Dude(5000, "Dealer")
 player = User(1000, "Player")
-
-
-""" def player_earns(amount: int) -> None:
-    player.chips += amount
-    dealer.chips -= amount """
 
 
 def play_round() -> None:
---
+++ deck.py
@@ -0,0 +1,18 @@
+class Card:
+    def __init__(self, value: int, name: str, suit: str) -> None:
+        self.value = value
+        self.name = name
+        self.suit = suit
+        self.in_deck = True
+    
+    def get_full_name(self) -> str:
+        return f"{self.name} of {self.suit}"
+
+deck = []
+
+for suit in ["hearts", "diamonds", "spades", "clubs"]:
+    deck.append(Card(11, "Ace", suit))
+    for i in range(2, 11):
+        deck.append(Card(i, str(i), suit))
+    for face in ["Jack", "Queen", "King"]:
+        deck.append(Card(10, face, suit))
