c43dec6d777587a696be173806da619b128f5265
Ghouljokes <gastreich.j@gmail.com>
2021-11-10 13:05:16 -0800

    move blackjack place_bet inside player class

--- blackjack.py
+++ blackjack.py
@@ -10,7 +10,6 @@ Stuff I need to do:
     organize the code relating to playing the game
 """
 deck = []
-id = 1
 class Card:
     def __init__(self, value: int, name: str, suit: str) -> None:
         self.value = value
@@ -21,6 +20,7 @@ class Card:
     def get_full_name(self) -> str:
         return f"{self.name} of {self.suit}"
 
+
 for suit in ["hearts", "diamonds", "spades", "clubs"]:
     deck.append(Card(11, "Ace", suit))
     for i in range(2, 11):
@@ -29,15 +29,25 @@ for suit in ["hearts", "diamonds", "spades", "clubs"]:
         deck.append(Card(10, face, suit))
 
 
-def place_bet() -> int:
-    bet = int(input("Place your bet: "))
-    if bet > player.chips:
-        print("You don't have enough chips!")
-        return place_bet()
-    if bet > dealer.chips:
-        print("Dealer doesn't have that many chips!")
-        return place_bet()
-    return bet
+class User(Dude):
+    def __init__(self, startingchips: int, name: str) -> None:
+        self.hand = []
+        self.chips = startingchips
+        self.name = name
+        self.bet = 0
+
+    def place_bet(self, to_match: object) -> None:
+        self.bet = int(input("Place your bet: "))
+        if self.bet > self.chips:
+            print("You don't have enough chips!")
+            self.place_bet(to_match)
+        if self.bet > to_match.chips:
+            print(f"{to_match.name} doesn't have that many chips!")
+            self.place_bet(to_match)
+
+
+dealer = Dude(5000, "Dealer")
+player = User(1000, "Player")
 
 
 def player_earns(amount: int) -> None:
@@ -48,8 +58,8 @@ def player_earns(amount: int) -> None:
 def play_round() -> None:
     print(f"House chips: {dealer.chips},  Player chips: {player.chips}")
     can_surrender = True
-    your_bet = place_bet()
-    can_double_down = your_bet * 2 <= player.chips
+    player.place_bet(dealer)
+    can_double_down = player.bet * 2 <= player.chips
     for card in deck:
         card.in_deck = True
     dealer.prep_round(deck)
@@ -57,13 +67,13 @@ def play_round() -> None:
     print(f"Dealer's hand: {dealer.hand[0].get_full_name()}, hidden")
 
     if dealer.get_total() == 21:
-        print(f"{dealer.show_hand()}\n Blackjack! Dealer wins {int(1.5 * your_bet)} chips.")
-        player_earns(int(-1.5 * your_bet))
+        print(f"{dealer.show_hand()}\n Blackjack! Dealer wins {int(1.5 * player.bet)} chips.")
+        player_earns(int(-1.5 * player.bet))
         return
 
     while player.get_total() < 21:
         print(player.show_hand())
-        print("Your bet: " + str(your_bet))
+        print("Your bet: " + str(player.bet))
         print("What would you like to do? Select a number:\n   1) hit\n   2) stay")
         if can_surrender:
             print("   3) surrender")
@@ -76,11 +86,11 @@ def play_round() -> None:
         elif choice == "2":
             break
         elif choice == "3" and can_surrender:
-            print(f"You surrender and lose {int(.5 * your_bet)} chips")
-            player_earns(int(-.5 * your_bet))
+            print(f"You surrender and lose {int(.5 * player.bet)} chips")
+            player_earns(int(-.5 * player.bet))
             return
         elif choice == "4" and can_double_down:
-            your_bet *= 2
+            player.bet *= 2
             player.draw(deck)
             break
         else:
@@ -88,12 +98,12 @@ def play_round() -> None:
     print(player.show_hand())
 
     if player.get_total() == 21:
-        print(f"Blackjack! You win {int(1.5 * your_bet)} chips.")
-        player_earns(int(1.5 * your_bet))
+        print(f"Blackjack! You win {int(1.5 * player.bet)} chips.")
+        player_earns(int(1.5 * player.bet))
         return
     elif player.get_total() > 21:
-        print(f"You overshoot and lose {your_bet} chips.")
-        player_earns(-your_bet)
+        print(f"You overshoot and lose {player.bet} chips.")
+        player_earns(-player.bet)
         return
 
     time.sleep(0.5)
@@ -103,20 +113,18 @@ def play_round() -> None:
     dealer.auto_draw(16, deck)
 
     if dealer.get_total() > 21:
-        print(f"House overshot. You gain {your_bet} chips")
-        player_earns(your_bet) 
+        print(f"House overshot. You gain {player.bet} chips")
+        player_earns(player.bet) 
     elif dealer.get_total() > player.get_total():
-        print(f"House wins. You lose {your_bet} chips")
-        player_earns(-your_bet)
+        print(f"House wins. You lose {player.bet} chips")
+        player_earns(-player.bet)
     elif dealer.get_total() < player.get_total():
-        print(f"You win. You gain {your_bet} chips")
-        player_earns(your_bet)
+        print(f"You win. You gain {player.bet} chips")
+        player_earns(player.bet)
     else:
         print("Evened out")
 
 
-player = Dude(1000, "Player")
-dealer = Dude(5000, "Dealer")
 while player.chips > 0 and dealer.chips > 0:
     play_round()
 if player.chips <= 0:
